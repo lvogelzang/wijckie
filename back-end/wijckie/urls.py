102b99a784d9cc7b3abb7fbc2cf783e8118841f4
Lodewijck Vogelzang <lodewijck@studiogoes.com>
2025-05-23 12:13:05 +0200

    Add REST endpoint

--- back-end/wijckie/urls.py
+++ back-end/wijckie/urls.py
@@ -16,8 +16,27 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.contrib.auth.models import User
+from django.urls import include, path
+from rest_framework import routers, serializers, viewsets
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ["url", "username", "email", "is_staff"]
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+router = routers.DefaultRouter()
+router.register(r"users", UserViewSet)
 
 urlpatterns = [
+    path("", include(router.urls)),
     path("admin/", admin.site.urls),
+    path("api-auth/", include("rest_framework.urls")),
 ]
