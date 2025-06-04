f648d2ca2974d79f0e6682e875822f074cd963bf
Lodewijck Vogelzang <lodewijck@studiogoes.com>
2025-06-04 14:39:06 +0200

    Fix startup

--- back-end/startup.sh
+++ back-end/startup.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-python manage.py collectstatic && gunicorn --workers 2 wijckie.wsgi
+python3 manage.py migrate
+python3 manage.py collectstatic --noinput
+gunicorn -w 3 -b :8000 wijckie.wsgi:application
