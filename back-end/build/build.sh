c9cfdbd74e11ffbf78b6531a000f816e0dcd7b19
Lodewijck Vogelzang <lodewijck@studiogoes.com>
2025-06-14 09:21:22 +0200

    Build container for AMD architecture

--- back-end/build/build.sh
+++ back-end/build/build.sh
@@ -12,8 +12,9 @@ NEXT_VERSION=$(echo ${LAST_USED_VERSION} | awk -F. -v OFS=. '{$NF += 1 ; print}'
 # NEXT_VERSION=2.0.1
 echo "Enter to continue building $NEXT_VERSION"
 read -n 1
-# docker build -t wijckie/back-end --platform linux/arm64 --no-cache -f ./Dockerfile ..
-docker build -t wijckie/back-end --platform linux/arm64 -f ./Dockerfile ..
+
+# docker build -t wijckie/back-end --platform linux/amd64 --no-cache -f ./Dockerfile ..
+docker build -t wijckie/back-end --platform linux/amd64 -f ./Dockerfile ..
 docker tag wijckie/back-end 557444162880.dkr.ecr.eu-central-1.amazonaws.com/wijckie/back-end:$NEXT_VERSION
 docker push 557444162880.dkr.ecr.eu-central-1.amazonaws.com/wijckie/back-end:$NEXT_VERSION
 aws lambda invoke \
